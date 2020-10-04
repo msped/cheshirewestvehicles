084726edb86d0a5e1abb3a19eb36b2c1b39b82f6
Matt <mattsp.edwards@googlemail.com>
2020-10-04 20:57:44 +0100

    added adminplus urls

--- cheshirewestvehicles/urls.py
+++ cheshirewestvehicles/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from adminplus.sites import AdminSitePlus
+
+admin.site = AdminSitePlus()
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
