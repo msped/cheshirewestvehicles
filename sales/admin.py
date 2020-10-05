d842f0046d6642e9f7067c52bc76e29fafdf78be
Matt <mattsp.edwards@googlemail.com>
2020-10-06 00:31:20 +0100

    added inline for vehicle images

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.shortcuts import render
+from .models import Vehicle, VehicleImages
 
 # Register your models here.
+
+class VehicleImagesInlineAdmin(admin.TabularInline):
+    model = VehicleImages
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    inlines = [VehicleImagesInlineAdmin]
+
+    class Meta:
+        model = Vehicle
+
+@admin.register(VehicleImages)
+class VehicleImagesAdmin(admin.ModelAdmin):
+    pass
