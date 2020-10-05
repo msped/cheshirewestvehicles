724d2ba41d26bcc3ee7a488f33a973f21aafdc97
Matt <mattsp.edwards@googlemail.com>
2020-10-06 00:30:31 +0100

    changed defaults removed order from images model

--- sales/models.py
+++ sales/models.py
@@ -38,11 +38,11 @@ class Vehicle(models.Model):
     make = models.CharField(max_length=15)
     model = models.CharField(max_length=15)
     trim = models.CharField(max_length=30)
-    year = models.IntegerField(choices=[(x, str(x)) for x in range(1980,datetime.date.today().year)], default=current_year)
+    year = models.IntegerField(choices=[(x, str(x)) for x in range(1980,datetime.date.today().year+1)], default=current_year)
     fuel = models.CharField(max_length=8, choices=Fuel.choices, default='1')
     body_type = models.CharField(max_length=8, choices=BodyType.choices, default='1')
     car_state = models.CharField(max_length=8, choices=CarState.choices, default='1')
-    reserved = models.CharField(max_length=8, choices=Reserve.choices, default='1')
+    reserved = models.CharField(max_length=8, choices=Reserve.choices, default='2')
     mileage = models.IntegerField()
     engine_size = models.IntegerField()
     mot_till = models.DateField()
@@ -56,7 +56,6 @@ class VehicleImages(models.Model):
     """Images relating to a vehicle"""
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='vehicle_images')
-    order = models.IntegerField()
 
     def __str__(self):
-        return f'{self.vehicle.id} - {self.order}'
+        return f'{self.vehicle.id} - {self.id}'
