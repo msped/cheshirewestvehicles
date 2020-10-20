9d91b72d8ba40a6b7af0374120003332a5c1503c
Matt <mattsp.edwards@googlemail.com>
2020-10-20 22:31:59 +0100

    added gallery and gallery detail views

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import galleryItem, galleryImages
 
 # Create your views here.
+
+def gallery(request):
+    featured = galleryItem.objects.all().order_by('-id')
+    images = galleryImages.objects.all()
+    context = {
+        'featured': featured,
+        'images': images
+    }
+    return render(request, "gallery.html", context)
+
+def gallery_detail(request, gallery_slug):
+    vehicle = galleryItem.objects.get(slug=gallery_slug)
+    images = galleryImages.objects.filter(item=vehicle)
+    context = {
+        'vehicle': vehicle,
+        'images': images
+    }
+    return render(request, "gallery_detail.html", context)
