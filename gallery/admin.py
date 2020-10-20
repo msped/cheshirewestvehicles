6af6da2cd3d51169b3bc0033d51f5065703ee04f
Matt <mattsp.edwards@googlemail.com>
2020-10-20 20:44:04 +0100

    added image inline to gallery model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import galleryItem, galleryImages
 
 # Register your models here.
+
+class galleryImagesInlineAdmin(admin.TabularInline):
+    model = galleryImages
+
+@admin.register(galleryItem)
+class galleryItemAdmin(admin.ModelAdmin):
+    inlines = [galleryImagesInlineAdmin]
+    prepopulated_fields = {'slug': ('make', 'model', 'trim', 'power')}
+
+    class Meta:
+        model = galleryItem
+
+@admin.register(galleryImages)
+class galleryImagesAdmin(admin.ModelAdmin):
+    pass
