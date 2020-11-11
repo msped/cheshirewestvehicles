5c7d909c8c00eb62e6b96d467c2768c22638f45f
Matt <mattsp.edwards@googlemail.com>
2020-11-11 23:58:43 +0000

    added view for create_invoice

--- business_admin/views.py
+++ business_admin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .utils import invoice_handler
 
 # Create your views here.
+
+def create_invoice(request):
+    if request.method == "POST":
+        invoice_handler(request)
+    return render(request, "create_invoice.html")
