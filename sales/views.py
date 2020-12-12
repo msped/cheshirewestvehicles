6c0133cb13bf43bb444af5e8acabae0190f0ad5e
Matt <mattsp.edwards@googlemail.com>
2020-12-12 23:45:18 +0000

    added trade in to email

--- sales/views.py
+++ sales/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from django.conf import settings
 from .models import Vehicle, VehicleImages
 from .forms import ReserveContactForm
 
@@ -42,12 +43,28 @@ def vehicle_detail(request, vehicle_slug):
                 request,
                 'You have reserved this vehicle. We will contact you for deposit payment.'
             )
+            standard_string = f'<p>Please contact {name} ({email}) on {phone_number} to reserve {vehicle.id} {vehicle.make} {vehicle.model} {vehicle.trim} £{vehicle.price}</p>'
+            if request.POST.get('trade-in', False):
+                make = request.POST.get('make')
+                model = request.POST.get('model')
+                trim = request.POST.get('trim')
+                year = request.POST.get('year')
+                mileage = request.POST.get('mileage')
+                comments = request.POST.get('comments')
+                standard_string += f' <p>Trade-in vehicle:</p>\
+                    <p>\
+                        Make: {make}\
+                        Model: {model}\
+                        Trim: {trim}\
+                        Year: {year}\
+                        Mileage: {mileage}\
+                        Comments: \n{comments}\
+                    </p>'
             # message = Mail(
             # from_email=settings.DEFAULT_FROM_EMAIL,
             # to_emails='autoskunkworks@gmail.com',
             # subject=f'Reserve vehicle request {vehicle.id} {vehicle.make} {vehicle.model} {vehicle.trim} £{vehicle.price}',
-            # html_content=f'<p>Please contact {name} ({email}) on {phone_number} to reserve {vehicle.id} {vehicle.make} {vehicle.model} {vehicle.trim} £{vehicle.price}</p>\
-            # <p>Please make sure to change the vehicle in the admin to "Reserved" so others can\'t request to reserve the vehicle.</p>')
+            # html_content=standard_string)
             # try:
             #     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
             #     response = sg.send(message)
