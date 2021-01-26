cda0c58fba3279e02a447181ace9850324334350
Matt <mattsp.edwards@googlemail.com>
2021-01-26 23:59:21 +0000

    added url for checking sale state

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Vehicles, VehicleDetail
+from .views import Vehicles, VehicleDetail, check_sale_state
 
 urlpatterns = [
     path('', Vehicles.as_view(), name="vehicles"),
-    path('<slug:vehicle_slug>/', VehicleDetail.as_view(), name="vehicle_detail")
+    path('<slug:vehicle_slug>/', VehicleDetail.as_view(), name="vehicle_detail"),
+    path('sale-state/<int:vehicle_id>', check_sale_state, name="get_sale_state"),
 ]
